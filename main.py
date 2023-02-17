13c55caa5b0a9b4a4108e4f5d48fbe25bebd2519
thomas <thomasgho@hotmail.com>
2023-02-17 14:51:55 +0900

    Refactor configuration routine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 
 import torch
@@ -16,8 +17,8 @@ def train(args):
     
     # load data
     for dataset in ["train", "test"]:
-        dp_ag = build_datapipe("data/ag/" + dataset)
-        dp_ab = build_datapipe("data/ab/" + dataset)
+        dp_ag = build_datapipe("data/ag/" + dataset, args.num_augment)
+        dp_ab = build_datapipe("data/ab/" + dataset, args.num_augment)
         
         # to iterate over two dataloaders simultaneously
         dp_zip = dp_ag.zip(dp_ab) 
@@ -72,7 +73,7 @@ def train(args):
         optimizer = optimizer,
         metric = metric,
         accumulation_steps = args.accumulation_steps,
-        log_dir = args.log_dir,
+        log_dir = "runs/" + args.exp_name,
         cuda = args.cuda,
         mixed_precision = args.mixed_precision,
     )
@@ -86,40 +87,19 @@ def train(args):
     
     # save model
     if args.save_model:
-        torch.save(model.state_dict(), args.log_dir)
+        torch.save(model.state_dict(), "runs/" + args.exp_name)
     
     
 if __name__ == '__main__':
-
+    
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--model", help="choose one of \"PECAN\" or \"PECAN_PN\"", type=str, default="PECAN_PN")
-    parser.add_argument(
-        "--in_feats", help="input feature dimension", type=int, default=62)
-    parser.add_argument(
-        "--hid_feats", help="hidden layers feature dimension", type=int, default=32)
-    parser.add_argument(
-        "--dropout", help="hidden layer dropout probability", type=float, default=0.5)
-    parser.add_argument(
-        "--lr", help="learning rate", type=float, default=0.001)
-    parser.add_argument(
-        "--weight_decay", help="L2 regularization", type=float, default=0.)
-    parser.add_argument(
-        "--momentum", help="Nesterov momentum factor", type=float, default=0.9)
-    parser.add_argument(
-        "--batch_size", help="train batch size", type=int, default=1)
-    parser.add_argument(
-        "--accumulation_steps", help="number of gradient accumilation steps", type=int, default=1)
-    parser.add_argument(
-        "--cuda", help="use CUDA acceleration", type=bool, default=True)
-    parser.add_argument(
-        "--mixed_precision", help="train with 16-bit and 32-bit floats to reduce memory overhead", type=bool, default=False)
-    parser.add_argument(
-        "--epochs", help="number of train epochs", type=int, default=100)
-    parser.add_argument(
-        "--log_dir", help="path to log tensorboard results", type=str, default="runs/exp1")
-    parser.add_argument(
-        "--save_model", help="whether to save model weights", type=bool, default=False)
+        "-f", "--exp_name", help="configuration json file name", type=str, default="exp1")
     args = parser.parse_args()
+    
+    with open("configs/" + args.exp_name + ".json", "r") as fp:
+        t_args = argparse.Namespace()
+        t_args.__dict__.update(json.load(fp))
+        args = parser.parse_args(namespace=t_args)
 
     train(args)
